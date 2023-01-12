f15ff06923b397e0f86816712a5828ab266c67f9
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-12 19:36:22 +0530

    third commit

--- project1/tailwind.config.js
+++ project1/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'bgImg': "url('/images/background.jpg')"
+      }
+    },
   },
   plugins: [],
 }
