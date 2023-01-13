f0b15be7f2d548d9402472b9c798dc4514a50eac
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-13 15:07:55 +0530

    fifth commit

--- project1/tailwind.config.js
+++ project1/tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
         poppins: 'Poppins',
         ubuntu: 'Ubuntu',
         laila: 'Laila'
+      },
+      boxShadow: {
+        shadoww: '-3px -3px 4px rgba(225, 225, 225,0.40),10px 5px 10px rgba(0,0,0,0.25)'
       }
     },
   },
