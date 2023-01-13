2dc533d586080ec667c9e96d878d847d9341eb57
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-13 17:42:21 +0530

    next commit

--- project1/tailwind.config.js
+++ project1/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       },
       boxShadow: {
         shadoww: '-3px -3px 4px rgba(225, 225, 225,0.40),10px 5px 10px rgba(0,0,0,0.25)'
+      },
+      transitionDuration:{
+        delay: '0.07s'
       }
     },
   },
