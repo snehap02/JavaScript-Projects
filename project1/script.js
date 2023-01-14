dd51ff2e7c40219309d8750cd778eb8387f5f9c6
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-14 17:42:54 +0530

    final commit

--- project1/script.js
+++ project1/script.js
@@ -6,15 +6,11 @@ window.addEventListener('keydown', function(e){
     audios.play();
 
     const keys = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    keys.style.width = '100px'
-    keys.style.height = '70px'
     keys.classList.add('playing');
 })
 
 window.addEventListener('keyup', function(e){
     const keys = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    keys.style.width = '112px'
-    keys.style.height = '96px'
     if(!keys) return;
     keys.classList.remove('playing');
 })
