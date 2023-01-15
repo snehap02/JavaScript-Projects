fec6b643fba36429cb3765c6ee1adb4bf9e79022
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-15 19:52:51 +0530

    new html changes

--- project2/tailwind.config.js
+++ project2/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        brown: '#301911',
+        lightbrown: '#4a391b'
+      }
+    },
   },
   plugins: [],
 }
