2e893b2f1412db254d96964f078a4a144be008c1
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-01-16 13:11:26 +0530

    fonts added

--- project2/tailwind.config.js
+++ project2/tailwind.config.js
@@ -6,8 +6,9 @@ module.exports = {
       colors:{
         brown: '#301911',
         lightbrown: '#4a3612',
-        brownyellow: '#d1a24f',
-        maddyBrown: '#574525'
+        brownyellow: '#e6b35a',
+        maddyBrown: '#574525',
+        newyellow: '#B27231'
       },
       backgroundImage:{
         'image': 'url(/images/back.jpg)'
