41482c7e98f22fb2c12d6033246b7dfae2506c91
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-03-17 22:43:05 +0530

    form designs

--- project4/script.js
+++ project4/script.js
@@ -7,6 +7,9 @@ const closee = document.getElementsByClassName('close');
 const signs = document.getElementsByClassName('sign');
 const container = document.getElementsByClassName('accord-container');
 const horizantal = document.getElementsByClassName('line');
+const mail = document.getElementById('email').value;
+const pass = document.getElementById('pass').value;
+const form = document.getElementById('form');
 
 
 selectField.addEventListener('click', () => {
@@ -30,4 +33,14 @@ for(let i = 0; i < accordion.length; i++){
             horizantal[i].classList.add('hidden');
         }
     })
+}
+
+function validation(){
+    if(mail == "" && pass == ""){
+        document.getElementById('email').classList.add('border-b-[3px]','border-[#e38400]');
+        document.getElementById('pass').classList.add('border-b-[3px]','border-[#e38400]');
+        document.getElementById('emailField').innerHTML = 'Please enter a valid email address or phone number.';
+        document.getElementById('passField').innerHTML = 'Your password must contain between 4 and 60 characters.';
+        return false;
+    }
 }
