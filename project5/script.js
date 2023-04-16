9e9eee6220f26f6ba35c7279b1026c75b57e5743
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-04-16 11:50:00 +0530

    final commit

--- project5/script.js
+++ project5/script.js
@@ -1,4 +1,24 @@
+const tab = document.querySelectorAll('.tab-item');
+const content = document.querySelectorAll('.tab-content-item');
 
+function selectItem(e){
+    removeCont();
+    removeBorder();
+    this.classList.add('border-l-4', 'border-[#6246ea]');
+    const showCont = document.querySelector(`#${this.id}-content`);
+    showCont.classList.add('show');
+}
+
+function removeCont(){
+    content.forEach(item => item.classList.remove('show'));
+}
+
+function removeBorder(){
+    tab.forEach(item => item.classList.remove('border-l-4', 'border-[#6246ea]'));
+}
+
+
+tab.forEach(item => item.addEventListener('click', selectItem));
 
 
 
--- project5/tailwind.config.js
+++ project5/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        '500': '500px'
+      }
+    },
   },
   plugins: [],
 }
